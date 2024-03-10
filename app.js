f0fdc37bc203a4867576f3a14db2bf09737beaaf
RaulTindogan <tindoganraul@gmail.com>
2024-03-10 10:45:36 +0800

    update styles and functions

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 let question = document.getElementsByClassName('questions')
-// let questionAnswers = document.getElementById('question-answer')
-
-// console.log(question[0].children[1].classList)
-
-
-
-// question[0].addEventListener('click', ()=>{
-//     question[0].children[1].classList.toggle('active')
-// })
-
 
 for(let i = 0; i<question.length; i++) {
     question[i].addEventListener('click', ()=>{
-       question[i].children[1].classList.toggle('active')
+       question[i].children[1].classList.toggle('active') 
+        if(question[i].children[1].classList[1] == 'active') {
+            question[i].children[0].children[1].setAttribute('src', 'assets/images/icon-minus.svg')
+        } else {
+            question[i].children[0].children[1].setAttribute('src', 'assets/images/icon-plus.svg')
+        }
     })
 }
 
